b13886b6a04ade883059dd7539e46b895bbc8c02
Grace Birnam <gbirnam@alphaworks.tech>
2022-03-18 11:54:55 -0700

    navbar links still not working

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const navSlide = () => {
             if(link.style.animation) {
                 link.style.animation = ''
             } else {
-                link.style.animation = `navLinkFade 0.5 ease forwards ${index / 7 +0.3}s`
+                link.style.animation = `navLinkFade 0.5 ease forwards ${index / 9 +0.3}s`
         
             }
         });
